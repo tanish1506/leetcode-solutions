4219541bdaf192d732fd07f39a183063001b01cb
Tanish Mishra <mishratanish780@gmail.com>
2026-01-07 01:39:40 +0530

    Time: 52 ms (21.82%) | Memory: 85 MB (35.19%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -8,7 +8,7 @@ public:
         while(r<n){
             mp[fruits[r]]++;
 
-            if(mp.size() > 2){
+            while(mp.size() > 2){
                 mp[fruits[l]]--;
                 if(mp[fruits[l]] == 0){
                     mp.erase(fruits[l]);
