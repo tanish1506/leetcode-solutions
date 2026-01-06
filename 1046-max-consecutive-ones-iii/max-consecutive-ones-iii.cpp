eab5e5afbd5577cae6c45fdb128d0a050b40c6a7
Tanish Mishra <mishratanish780@gmail.com>
2026-01-07 00:51:17 +0530

    Time: 0 ms (100.00%) | Memory: 69.5 MB (90.79%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -9,7 +9,7 @@ public:
         while(r<n){
             if(nums[r] == 0) zero++;
 
-            while(zero > k){
+            if(zero > k){
                 if(nums[l] == 0) zero--;
                 l++;
             }
