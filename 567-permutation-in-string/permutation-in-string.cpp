f7953ccafb2f5d97c41bce8ffe8f06cf48a9816c
Tanish Mishra <mishratanish780@gmail.com>
2026-01-08 23:48:46 +0530

    Time: 3 ms (67.19%) | Memory: 9.8 MB (86.73%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        if(n>m) return false;
+
+        vector<int> freq1(26,0);
+        vector<int> freq2(26,0);
+
+        for(int i=0;i<n;i++){
+            freq1[s1[i] - 'a']++;
+        }
+
+        for(int i=0;i<m;i++){
+            freq2[s2[i] - 'a']++;
+            if(i >= n){
+                freq2[s2[i-n] - 'a']--;
+
+            }
+            if(freq1 == freq2) return true;
+        }
+        return false;
+
+    }
+};
