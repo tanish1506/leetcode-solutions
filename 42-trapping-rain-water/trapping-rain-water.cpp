cd8d9d1774d88c5dcc32524b6ecd2cbe3b90e44b
Tanish Mishra <mishratanish780@gmail.com>
2026-01-11 00:17:42 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (58.17%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n= height.size();
-        vector<int> leftM(n);
-        vector<int> rightM(n);
-        leftM[0] = height[0];
-        rightM[n-1] = height[n-1];
-
-        for(int i=1;i<n;i++){
-            leftM[i] = max(leftM[i-1] , height[i]);
-        }
-        for(int i=n-2;i>=0;i--){
-            rightM[i] = max(rightM[i+1],height[i]);
-        }
-
-        int wtrtrap = 0;
-        for(int i=0;i<n;i++){
-            int currwtr = min(leftM[i],rightM[i]) - height[i];
-            if(currwtr > 0){
-                wtrtrap += currwtr;
+        int n = height.size();
+        int l=0,r=n-1,leftM=0,rightM=0,water=0;
+        while(l<r){
+            if(height[l] <= height[r]){
+                if(height[l] >= leftM){
+                    leftM = height[l];
+                }else{
+                    water += leftM - height[l];
+                }
+                l++;
+            }
+            else{
+                if(height[r] >= rightM){
+                    rightM = height[r];
+                }
+                else{
+                    water += rightM - height[r];
+                }
+                r--;
             }
         }
-        return wtrtrap;
-
+        return water;
     }
 };
