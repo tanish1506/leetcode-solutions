9b0acc080388768cf7e55b3dc8e95f32a41711b2
Tanish Mishra <mishratanish780@gmail.com>
2026-01-20 23:25:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (48.04%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        vector<int> freq(26,0);
+
+        for(char c : s) freq[c - 'a']++;
+        for(char c : t) freq[c - 'a']--;
+
+        for(int i=0;i<freq.size();i++){
+            if(freq[i] != 0) return false;
+        }
+        return true;
+    }
+};
