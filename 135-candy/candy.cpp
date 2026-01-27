d2183d13341ecf9951007168220a535604f21568
Tanish Mishra <mishratanish780@gmail.com>
2026-01-28 01:46:03 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (92.21%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+
+        int sum=1,i=1;
+        while(i<n){
+            if(ratings[i] == ratings[i-1]){
+                sum += 1;
+                i++;
+                continue;
+            }
+            int peak = 1;
+            while(i<n && ratings[i] > ratings[i-1]){
+                peak++;
+                sum += peak;
+                i++;
+            }
+            int down=1;
+            while(i<n && ratings[i] < ratings[i-1]){
+                sum += down;
+                down++;
+                i++;
+            }
+
+            if(down > peak){
+                sum += (down - peak);
+            }
+        }
+        return sum;
+    }
+};
