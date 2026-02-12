233d617bbfbe15a2e19d39f0b1dfffc39b7abde5
Tanish Mishra <mishratanish780@gmail.com>
2026-02-13 01:20:51 +0530

    Time: 3 ms (53.70%) | Memory: 14.1 MB (57.77%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    void findComb(int idx,vector<int> &nums,int target, vector<int> &curr, vector<vector<int>> & ans){
-        if(idx == nums.size()){
-            if(target == 0){
-                ans.push_back(curr);
-            }
-            return;
+void findComb(int idx,vector<int> &arr,int target,vector<int> & curr, vector<vector<int>> &ans){
+    if(idx == arr.size()){
+        if(target == 0){
+            ans.push_back(curr);
         }
-
-        //pick the element
-        if(nums[idx] <= target){
-            curr.push_back(nums[idx]);
-            findComb(idx,nums,target - nums[idx],curr, ans);
-            curr.pop_back();
-        }
-        findComb(idx+1,nums,target,curr,ans);
+        return;
     }
+    //pick
+    if(arr[idx] <= target){
+        curr.push_back(arr[idx]);
+        findComb(idx,arr,target-arr[idx],curr,ans);
+        curr.pop_back();
+    }
+    //not pick
+    findComb(idx+1,arr,target,curr,ans);
+}
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
         vector<int> curr;
+        vector<vector<int>> ans;
         findComb(0,candidates,target,curr,ans);
         return ans;
     }
