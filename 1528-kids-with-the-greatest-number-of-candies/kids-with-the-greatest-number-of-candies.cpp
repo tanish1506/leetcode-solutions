e9ab8c284f3371fa690d36bc5a491c4a9cf5d197
Tanish Mishra <mishratanish780@gmail.com>
2026-06-03 23:59:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (58.55%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies){
         vector<bool> res(candies.size());
-        int max = 0;
+        int maxi=0;
         for(int i=0;i<candies.size();i++){
-            if(max < candies[i]){
-                max = candies[i];
-            }
+            maxi = max(maxi,candies[i]);
         }
         for(int i=0;i<candies.size();i++){
-            int extra = candies[i] + extraCandies;
-            if(extra >= max){
+            int extra = candies[i]+extraCandies;
+            if( extra >= maxi){
                 res[i] = true;
             }
         }
