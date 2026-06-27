83a269d83391f7717234d59502c45d0d279152a0
Tanish Mishra <mishratanish780@gmail.com>
2026-06-27 23:52:03 +0530

    Time: 2 ms (16.43%) | Memory: 14.1 MB (82.82%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        stack<int> st;
+        int n = prices.size();
+        vector<int> ans(n);
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && st.top() > prices[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                ans[i] = prices[i];
+            }else{
+                ans[i] = prices[i] - st.top();
+            }
+            st.push(prices[i]);
+        }
+        return ans;
+    }
+};
