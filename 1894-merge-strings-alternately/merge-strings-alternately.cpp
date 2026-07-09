8e7fbdbef32c721cb9ca9e8969d641cafc4c024e
Tanish Mishra <mishratanish780@gmail.com>
2026-07-09 23:47:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (10.88%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int i=0,j=0,k=0;
         int n = word1.size();
         int m = word2.size();
         string res;
         res.resize(n+m);
+        int i=0,j=0,k=0;
         while(i < n && j < m){
             if(k%2 == 0){
                 res[k++] = word1[i++];
-            }
-            else{
+            }else{
                 res[k++] = word2[j++];
             }
         }
@@ -20,7 +19,6 @@ public:
         while(j<m){
             res[k++] = word2[j++];
         }
-
         return res;
     }
 };
