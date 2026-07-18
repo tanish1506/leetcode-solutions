51ba0d8e8c252c4fee3551cfb659cd52431d4fae
Tanish Mishra <mishratanish780@gmail.com>
2026-07-19 00:25:09 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (31.74%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
@@ -6,7 +6,7 @@ public:
         int small = nums[0];
         int large = nums[n-1];
         
-        for(int i=small;i>=0;i--){
+        for(int i=small;i>0;i--){
             if(small % i == 0 && large % i == 0){
                 return i;
             }
