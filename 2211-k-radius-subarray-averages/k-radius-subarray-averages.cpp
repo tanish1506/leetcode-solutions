c45383c3877555c831b172d726a6bac292c10f44
Tanish Mishra <mishratanish780@gmail.com>
2026-08-05 02:07:18 +0530

    Time: 0 ms (100.00%) | Memory: 134.4 MB (40.32%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
@@ -3,22 +3,22 @@ public:
     vector<int> getAverages(vector<int>& nums, int k) {
         int n = nums.size();
         vector<int> avg(n,-1);
-
-        int windowsize = 2*k+1;
-        if(windowsize > n) return avg;
-
+        int wdw = 2*k+1;
         long long sum = 0;
-        for(int i=0;i<windowsize;i++){
+
+        if(wdw > n)return avg;
+
+        for(int i=0;i<wdw;i++){
             sum += nums[i];
         }
+        avg[k] = sum/wdw;
 
-        avg[k] = sum/windowsize;
-
-        for(int i=windowsize;i<n;i++){
+        for(int i=wdw;i<n;i++){
             sum += nums[i];
-            sum -= nums[i - windowsize];
-            avg[i-k] = sum/windowsize;
+            sum -= nums[i-wdw];
+            avg[i-k] = sum/wdw;
         }
+
         return avg;
     }
 };
