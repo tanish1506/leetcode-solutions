ae4dc850fd29fdbe60d985c68899a74710d2d737
Tanish Mishra <mishratanish780@gmail.com>
2026-09-03 23:58:42 +0530

    Time: 8 ms (47.38%) | Memory: 166 MB (31.94%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int mn = INT_MAX;
+
+        for(int x : nums1){
+            if(x % 2 == 1){
+                mn =  min(mn,x);
+            }
+        }
+
+        if(mn == INT_MAX){
+            return true;
+        }
+
+        for(int x : nums1){
+            if(x%2 == 0 && x < mn){
+                return false;
+            }
+        }
+        return true;
+    }
+};
