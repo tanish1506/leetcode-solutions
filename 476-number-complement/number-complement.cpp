a0d5fac2bf4c315abbc1cec13b2f6cce641b98d7
Tanish Mishra <mishratanish780@gmail.com>
2026-09-16 11:56:48 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (43.37%) - LeetSync

---
+++ 476-number-complement/number-complement.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int m = num;
+        int mask = 0;
+        while(m != 0){
+            mask =  (mask << 1) | 1;
+            m = m >> 1;
+        }
+        int ans = (~num) & mask;
+        return ans;
+    }
+};
