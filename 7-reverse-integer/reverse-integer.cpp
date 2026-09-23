3711011b28460f75f8819fa44be9edb1291f77c5
Tanish Mishra <mishratanish780@gmail.com>
2026-09-23 11:43:06 +0530

    Time: 3 ms (29.78%) | Memory: 8.4 MB (83.11%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -7,8 +7,8 @@ public:
             if(ans > INT_MAX/10 || ans < INT_MIN/10){
                 return 0;
             }
-            ans = (ans * 10) + digit;
-            x=x/10;
+            ans = (ans*10)+digit;
+            x = x/10;
         }
         return ans;
     }
